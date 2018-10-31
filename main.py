f12c1e214ead0ae015e80efc0e9c6cad6d578a0e
Rodion Volovik <rodion.volovik@zenuity.com>
2018-10-31 11:36:36 +0200

    npuzzle

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
 
     if board.is_solvable():
         print("OOOPS! Puzzle is not solvable")
+        print(board)
         sys.exit()
 
     goal = generate_goal_puzzle(board.size)
